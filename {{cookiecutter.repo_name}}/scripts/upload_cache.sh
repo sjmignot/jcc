85926791220c568d315e73a79240d57717fddd7b
sjmignot <sjmignot@gmail.com>
2023-10-17 08:52:39 -0700

    make upload and download cache accessible through poetry

--- {{cookiecutter.repo_name}}/scripts/upload_cache.sh
+++ {{cookiecutter.repo_name}}/scripts/upload_cache.sh
@@ -1,14 +1,9 @@
 docker build . -t {{cookiecutter.repo_name}} --ssh default
 
-random_port={{ range(2000, 65000) | random }}
-
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=http://localhost:$random_port/lab/ &
-
 docker run -it \
 	-v $GOOGLE_APPLICATION_CREDENTIALS:/tmp/keys/key.json:ro \
 	-v $PWD/.cache:/.cache \
 	-v ./output:/output \
 	-v $PWD/{{cookiecutter.repo_name}}:/{{cookiecutter.repo_name}} \
 	-e GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/key.json \
-	-p $random_port:8888 \
-	{{cookiecutter.repo_name}}
+	poetry run upload_cache
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/bash_runner.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/bash_runner.py
@@ -10,3 +10,11 @@ def _run(bash_script):
 
 def docker_run():
     return _run(os.path.join(os.path.dirname(DIR_PATH), "scripts", "docker_run.sh"))
+
+
+def upload_cache():
+    return _run(os.path.join(os.path.dirname(DIR_PATH), "scripts", "upload_cache.sh"))
+
+
+def download_cache():
+    return _run(os.path.join(os.path.dirname(DIR_PATH), "scripts", "download_cache.sh"))
